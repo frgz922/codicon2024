5a9ce70e9702990c3194985d650bc37fc72e0725
Luis Galban <luis.egr.342@gmail.com>
2024-03-17 07:34:32 -0400

    Shorter
    Shorter

--- codicon2024/transmission/urls.py
+++ codicon2024/transmission/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('create/', create_transmission, name='create_transmission'),
     path('list/', transmission_list, name='transmission_list'),
     path('<int:transmission_id>/', transmission_detail, name='transmission_detail'),
-    path('s/<str:short_id>/', transmission_detail_short, name='transmission_detail_short'),
+    path('s/<short_id>/', transmission_detail_short, name='transmission_detail_short'),
 ]
